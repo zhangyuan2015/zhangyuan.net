c47e3c4198924549777c88b6731fd9bd880a8a95
zhangyuan <cool.zhangyy@gmail.com>
2020-11-08 03:45:20 +0800

    样式优化

--- static/js/LookBackGallery.js
+++ static/js/LookBackGallery.js
@@ -1,18 +1,32 @@
 $(document).ready(function () {
-    $.get("http://23.105.193.208:5000/LookBackGallery?year=2020", function (data, status) {
-        console.log("Status: " + status);
-        $('#template1').tmpl(data).appendTo('#timeline-js')
+	$.get("http://23.105.193.208:5000/LookBackGallery?year=2020", function (data, status) {
+		console.log("Status: " + status);
+		$('#template1').tmpl(data).appendTo('#timeline-js')
 
-        $.timeliner({});
-			$.timeliner({
-				timelineContainer: '#timeline-js',
-				timelineSectionMarker: '.milestone',
-				oneOpen: true,
-				startState: 'open',
-				expandAllText: '+ 展开所有',
-				collapseAllText: '- 折叠所有'
-			});
-			// Colorbox Modal
-			$(".CBmodal").colorbox({ rel: 'group', transition: "fade", width: "90%", height: "90%" });
-    });
+		$("#timeline-js").css({
+			"overflow": "auto",
+			"width": "auto"
+		});
+
+		$("#timeline-js").css({
+			"overflow": "auto",
+			"width": "auto"
+		});
+
+		$(".container").css({
+			"width": "960px"
+		});
+
+		$.timeliner({});
+		$.timeliner({
+			timelineContainer: '#timeline-js',
+			timelineSectionMarker: '.milestone',
+			oneOpen: true,
+			startState: 'open',
+			expandAllText: '+ 展开所有',
+			collapseAllText: '- 折叠所有'
+		});
+		// Colorbox Modal
+		$(".CBmodal").colorbox({ rel: 'group', transition: "fade", width: "90%", height: "90%" });
+	});
 });
