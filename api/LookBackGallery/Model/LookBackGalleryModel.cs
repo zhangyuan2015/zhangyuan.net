365f778ae4f9aee361161f9f5afc1d997b12dc34
zhangyuan <cool.zhangyy@gmail.com>
2020-11-08 03:12:13 +0800

    模型优化

--- api/LookBackGallery/Model/LookBackGalleryModel.cs
+++ api/LookBackGallery/Model/LookBackGalleryModel.cs
@@ -4,21 +4,21 @@ namespace api.LookBackGallery.Model
 {
     public class LookBackGalleryModel
     {
-        public int Year { get; set; }
+        public string Year { get; set; }
 
         public List<LookBackGalleryMonthModel> MonthList { get; set; }
     }
 
     public class LookBackGalleryMonthModel
     {
-        public int Month { get; set; }
+        public string Month { get; set; }
 
         public List<LookBackGalleryDayModel> DayList { get; set; }
     }
 
     public class LookBackGalleryDayModel
     {
-        public int Day { get; set; }
+        public string Day { get; set; }
 
         public List<string> UrlList { get; set; }
     }
